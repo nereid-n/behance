d42b00da5169e750862856ff7e96443b876802e8
nereid <nas.kuleshova@gmail.com>
2018-11-02 17:51:16 +0300

    end desktop

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -49,4 +49,12 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+if ($('.reviews__slider').length > 0) {
+  $('.reviews__slider').slick({
+    slidesToShow: 3,
+    prevArrow: '<button class="reviews__btn reviews__prev"><img src="assets/images/icons/arrow-left.png" alt=""></button>',
+    nextArrow: '<button class="reviews__btn reviews__next"><img src="assets/images/icons/arrow-right.png" alt=""></button>'
+  });
+}
 //# sourceMappingURL=script.js.map
---
+++ src/components/prices/prices.js
@@ -0,0 +1,7 @@
+if ($('.reviews__slider').length > 0) {
+  $('.reviews__slider').slick({
+    slidesToShow: 3,
+    prevArrow: '<button class="reviews__btn reviews__prev"><img src="assets/images/icons/arrow-left.png" alt=""></button>',
+    nextArrow: '<button class="reviews__btn reviews__next"><img src="assets/images/icons/arrow-right.png" alt=""></button>'
+  });
+}
