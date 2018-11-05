16c4412bfec91312dc9e27b1b61f523cac1be361
nereid <nas.kuleshova@gmail.com>
2018-11-05 15:28:48 +0300

    adaptive

---
+++ src/components/reviews/reviews.js
@@ -0,0 +1,21 @@
+if ($('.reviews__slider').length > 0) {
+  $('.reviews__slider').slick({
+    slidesToShow: 3,
+    prevArrow: '<button class="reviews__btn reviews__prev"><img src="assets/images/icons/arrow-left.png" alt=""></button>',
+    nextArrow: '<button class="reviews__btn reviews__next"><img src="assets/images/icons/arrow-right.png" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+}
